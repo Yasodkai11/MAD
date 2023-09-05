5c2039fc3eb0d252a5d01830564162ce8db01f96
Chamodya Sewwandi Gamage <Chamodyasewwandi99@gmail.com>
2023-09-05 09:11:11 +0530

    add

--- unity_world/lib/main.dart
+++ unity_world/lib/main.dart
@@ -7,13 +7,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '',
       theme: ThemeData(
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
