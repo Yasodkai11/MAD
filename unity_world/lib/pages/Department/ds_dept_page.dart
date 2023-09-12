e6759d0c0070dbbda68fa692ac398c44f8fb744e
Yasod Kavindu de Silva <97452320+Yasodkai11@users.noreply.github.com>
2023-09-12 13:45:40 +0530

    deptclass

---
+++ unity_world/lib/pages/Department/ds_dept_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DsDeptPage extends StatelessWidget {
+  const DsDeptPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ unity_world/lib/pages/Department/mis_dept_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IsseDepPage extends StatelessWidget {
+  const IsseDepPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
