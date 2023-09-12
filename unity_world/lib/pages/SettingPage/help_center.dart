d337b9ca41c575d7c0de1af0241a8b4a6d217205
Chamodya Sewwandi Gamage <Chamodyasewwandi99@gmail.com>
2023-09-12 09:39:57 +0530

    three pages add

---
+++ unity_world/lib/pages/SettingPage/help_center.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:quickalert/quickalert.dart';
+
+class HelpCenter extends StatefulWidget {
+  const HelpCenter({Key? key}) : super(key: key);
+
+  @override
+  State<HelpCenter> createState() => _HelpCenterState();
+}
+
+
+class _HelpCenterState extends State<HelpCenter> {
+ 
+
+
+void showAlert(){
+  QuickAlert.show(
+    context: context, 
+    type: QuickAlertType.success
+  );
+}
+
+  @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('Help Center Page'),
+      ),
+
+
+      
+
+
+
+
+  
+          
+
+
+  
+  
+        
+    
+  
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+ 
+        );
+   }                    
+   }
