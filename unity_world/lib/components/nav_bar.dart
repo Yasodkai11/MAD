cad54bfc907518c51dbdcc1207dfd42c58d2c009
Sanali Losathi <kklosathi19@gmail.com>
2023-09-12 19:57:18 +0530

    navBar method

---
+++ unity_world/lib/components/nav_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatefulWidget {
+
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold();
+    
+  }
+}
