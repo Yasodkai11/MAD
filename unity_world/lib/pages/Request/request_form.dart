89ada3de5c89201c56be8fdc442a1c70a3c0c973
ridmanavod <mlrnavod@students.nsbm.ac.lk>
2023-09-14 14:07:21 +0530

    edgeinsetrs

--- unity_world/lib/pages/Request/request_form.dart
+++ unity_world/lib/pages/Request/request_form.dart
@@ -45,6 +45,14 @@ class _requestState extends State<request> {
               ],
             ),
             const SizedBox(height: 10.0),
+            Container(
+              margin: const EdgeInsets.only(
+                top: 10,
+                bottom: 5,
+                right: 50,
+                left: 10,
+              ),              
+            ),
           ],
         ),
       ),
