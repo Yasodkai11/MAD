3ca29be86eb9e760b6ad23f194763f557c7e9b8b
ridmanavod <mlrnavod@students.nsbm.ac.lk>
2023-09-15 18:53:10 +0530

    21.1

--- unity_world/lib/pages/Schedule/shedule_page.dart
+++ unity_world/lib/pages/Schedule/shedule_page.dart
@@ -142,6 +142,20 @@ class _SheduleState extends State<Shedule> {
                     width: 20,
                     color: Colors.white,
                   ),
+                  Container(
+                    height: 54,
+                    width: 98,
+                    color: const Color.fromARGB(255, 15, 26, 88),
+                    child: const Center(
+                      child: Text(
+                        "21.1",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ],
