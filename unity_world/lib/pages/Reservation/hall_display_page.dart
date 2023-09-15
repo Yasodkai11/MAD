9216acd3be0aeb880f2dbb3c07368e174bd5fbaa
ThenukaW <98868025+ThenukaW@users.noreply.github.com>
2023-09-16 03:13:10 +0530

    added:hall reservation

---
+++ unity_world/lib/pages/Reservation/hall_display_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ReservationPage extends StatelessWidget {
+ const ReservationPage({super.key});
+
+ @override
+ Widget build (BuildContext context ){
+  return Scaffold();
+ }
+
+}
