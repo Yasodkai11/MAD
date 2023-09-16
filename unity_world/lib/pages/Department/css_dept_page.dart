fc7aa2d929a13d47d8ce62240c789196ce81084d
Yasod Kavindu de Silva <97452320+Yasodkai11@users.noreply.github.com>
2023-09-16 19:07:01 +0530

    imagecurve

--- unity_world/lib/pages/Department/css_dept_page.dart
+++ unity_world/lib/pages/Department/css_dept_page.dart
@@ -15,10 +15,13 @@ class CsseDepPage extends StatelessWidget {
             child: Center(
               child: Column(
                 children: [
-                  Image.asset(
+                  ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child:Image.asset(
                     'assets/images/csse.png',
                     height: 200,
                     width: 1000,
+                  ),
                   ),
                   _buildCurvedContainer(
                     context,
