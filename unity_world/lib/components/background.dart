8bd24ca94579dc7bda2a9ae6cee0e5e39aa3fd9f
Sanali Losathi <kklosathi19@gmail.com>
2023-09-17 06:55:47 +0530

    updated background file

---
+++ unity_world/lib/components/background.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  const Background({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
