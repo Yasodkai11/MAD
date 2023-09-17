adeafde680e646714b47c85d7cde8962a5a37a73
Chamodya Sewwandi Gamage <Chamodyasewwandi99@gmail.com>
2023-09-17 10:45:07 +0530

    delete continue page

--- unity_world/lib/pages/Welcome/continue_page.dart
+++ unity_world/lib/pages/Welcome/continue_page.dart
@@ -7,121 +7,67 @@ class ContinuePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      
 
-        children: [
-          const Padding(
-            padding: 
-              EdgeInsets.fromLTRB(40, 0, 0 ,0),
-            child: 
-              Align(
-                alignment: Alignment.topLeft,
-                child: 
-                  Text(
-                    "Continue As......",
-                  style: 
-                    TextStyle(
-                      color: Color.fromRGBO(30, 144, 255, 1.0),
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                  ),
-                ),
-            ),
-          ),
+   
 
-          Image.asset(
-            'assets/illustrations/user.png',
-              height: 170,
-              width: 500,
-          ),
 
-          Padding(
-            padding: 
-              const EdgeInsets.all(8.0),
-            child: 
-              Align(
-                alignment: Alignment.center,
-                child: 
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginPage(),
-                        )
-                      );
-                      print(
-                        'Login as a User'
-                      );
-                    }, 
-                    style: 
-                      ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromRGBO(15, 26, 88, 1.0),
-                        fixedSize: const Size(300, 5),
-                        shape: const RoundedRectangleBorder(),
-                    ),
-                    child: 
-                      const Text(
-                        "A User",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400,
-                          ),
-                    )
-                )
-            ),
-          ),
 
-          Image.asset(
-            'assets/illustrations/admin.png',
-              height: 170,
-              width: 500,
-          ),
 
-          Padding(
-            padding: 
-              const EdgeInsets.all(8.0),
-            child: 
-              Align(
-                alignment: Alignment.center,
-                child: 
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginPage()),
-                      );
-                      print(
-                        'Login as an Admin'
-                      );
-                    },
-                    style: 
-                      ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromRGBO(15, 26, 88, 1.0),
-                        fixedSize: const Size(300, 5),
-                        shape: const RoundedRectangleBorder(),
-                    ), 
-                    child: 
-                      const Text(
-                        "An Administrator",
-                          style: 
-                            TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400,
-                          ),
-                    ),
-              ),
-            ),
-          ),
 
-        ],
 
-      ),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
     );
   }
 
