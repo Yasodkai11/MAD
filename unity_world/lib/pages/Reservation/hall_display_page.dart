da22f578329a30724e7e76db00d696e451ecdc38
ThenukaW <98868025+ThenukaW@users.noreply.github.com>
2023-09-17 19:02:51 +0530

    modified reservation

--- unity_world/lib/pages/Reservation/hall_display_page.dart
+++ unity_world/lib/pages/Reservation/hall_display_page.dart
@@ -12,3 +12,6 @@ class ReservationPage extends StatefullWidget {
 class _ReservationPageState extends State<ReservationPage> {
   int selectedHall = 0;
   int selectedTime = 0;
+
+  final List<String> hallNumbers = ['C1-L101', 'C1-005', 'C1-006','C1-007','C1-008']; 
+  final List<String> reservedTimes = ['9:00 AM-12:00AM', 'Avaiable', '2:00 PM-4:00pm','Avaiable','Avaiable'];
