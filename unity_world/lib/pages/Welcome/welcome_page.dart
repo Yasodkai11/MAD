126b39d00e7a07debfd5f863765491a76418d538
Chamodya Sewwandi Gamage <Chamodyasewwandi99@gmail.com>
2023-09-17 10:40:34 +0530

    delete welcome page

--- unity_world/lib/pages/Welcome/welcome_page.dart
+++ unity_world/lib/pages/Welcome/welcome_page.dart
@@ -8,72 +8,38 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      backgroundColor: Colors.white,
+     
 
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
 
-        children: [
 
-          const Expanded(
 
-            child: Center(
-              child: Text(
-                "WELCOME TO",
-                style: TextStyle(
-                  color: Color.fromRGBO(15, 26, 88, 1),
-                  fontSize: 52,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
 
-          ),
 
-          Expanded(
-            
-            child: Column(
-              children: [
-                Center(
-                  child: Image.asset(
-                    'assets/icons/logo1.png',
-                    height: 250,
-                    width: 250,
-                  ),
-                ),
-              ],
-            ),
 
-          ),
 
-          Padding(
-            padding: 
-              const EdgeInsets.fromLTRB(0, 0, 10, 55),
-            child: 
-              Align(
-                alignment: Alignment.bottomRight,
 
-                child: 
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context, 
-                          MaterialPageRoute(
-                            builder: 
-                              (context) => const ContinuePage())
-                      );
-                    }, 
-                    child: 
-                      const Icon(
-                        Icons.navigate_next_rounded
-                      ),
-                  ),
 
-              ),
-          ),
 
-        ],
-      ),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
 
     );
   }
