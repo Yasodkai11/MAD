c05dd63600dfc6d5873dd321b46f3ec083233576
Sanali Losathi <kklosathi19@gmail.com>
2023-09-17 07:05:20 +0530

    updated back appBar file

---
+++ unity_world/lib/components/back_notifi_account.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BackAppBar extends StatelessWidget {
+  const BackAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar();
+  }
+}
