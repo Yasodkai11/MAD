c9728a156596454f19567624bbaf1312c51de521
Yasod Kavindu de Silva <97452320+Yasodkai11@users.noreply.github.com>
2023-09-17 07:21:32 +0530

    dslogocurved

--- unity_world/lib/pages/Department/ds_dept_page.dart
+++ unity_world/lib/pages/Department/ds_dept_page.dart
@@ -15,11 +15,14 @@ class DsDeptPage extends StatelessWidget {
             child: Center(
               child: Column(
                 children: [
-                  Image.asset(
+                   ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
                     'assets/images/csds.jpg',
                     height: 200,
                     width: 1000,
                   ),
+                   ),
                    _buildCurvedContainer(
                     context,
                   Container(
