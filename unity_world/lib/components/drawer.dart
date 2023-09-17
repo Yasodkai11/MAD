eaddafb50bf0eeb5d75a0dabd9cc72cc38e185c5
Sanali Losathi <kklosathi19@gmail.com>
2023-09-17 08:59:49 +0530

    modified drawer

--- unity_world/lib/components/drawer.dart
+++ unity_world/lib/components/drawer.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
-  const MyDrawer({super.key});
+  const MyDrawer({
+    super.key,
+    required Container child 
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    Size size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        topRight: Radius.circular(15),
+        bottomRight: Radius.circular(15),
+      ),
+      
+      child: Drawer()
+    );
   }
   
 }
