3a3005a84eed967bbce23b5fae1efa2c9140d936
Yasod Kavindu de Silva <97452320+Yasodkai11@users.noreply.github.com>
2023-09-18 11:32:02 +0530

    imagecurved2

--- unity_world/lib/pages/Department/dns_dept_page.dart
+++ unity_world/lib/pages/Department/dns_dept_page.dart
@@ -140,11 +140,14 @@ class DnsDeptPage extends StatelessWidget {
                       ),
                       Column(
                         children: [
-                          Image.asset(
+                           ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                          child: Image.asset(
                             'assets/images/dilhara.webp',
                             width: 100,
                             height: 100,
                           ),
+                           ),
                           const Text(
                             'Mr. Dilhara Batan Arachchige\nLecturer',
                             textAlign: TextAlign.center,
