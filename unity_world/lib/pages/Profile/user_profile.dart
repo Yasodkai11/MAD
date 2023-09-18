a3f0aff57c03a922277ac27eff175d1c289fac73
Kavee2000 <99233218+Kavee2000@users.noreply.github.com>
2023-09-18 15:06:07 -0700

    profile1

--- unity_world/lib/pages/Profile/user_profile.dart
+++ unity_world/lib/pages/Profile/user_profile.dart
@@ -14,6 +14,17 @@ class UserProfile extends StatelessWidget {
 
 
 
-
-    
-  
+     Future<void> _showLogoutConfirmationDialog(BuildContext context) async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Confirm Logout'),
+            content: const SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text('Are you sure you want to logout?'),
+                ],
+              ),
+            ),
