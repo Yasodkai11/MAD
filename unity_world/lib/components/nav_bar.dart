e111b6dc017bd3685a33d78aa7a49566a9cb393b
Yasod Kavindu de Silva <97452320+Yasodkai11@users.noreply.github.com>
2023-09-18 15:18:56 +0530

    changepage

--- unity_world/lib/components/nav_bar.dart
+++ unity_world/lib/components/nav_bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:unity_world/pages/Club/club_main.dart';
+import 'package:unity_world/pages/Club/community_page.dart';
 import 'package:unity_world/pages/HomePage/home_page.dart';
 import 'package:unity_world/pages/Request/request_form.dart';
 import 'package:unity_world/pages/Schedule/shedule_page.dart';
