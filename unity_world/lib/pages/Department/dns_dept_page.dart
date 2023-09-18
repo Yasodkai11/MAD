7573c042ea5840d197a2d6699bf782d1a4db5407
Yasod Kavindu de Silva <97452320+Yasodkai11@users.noreply.github.com>
2023-09-18 11:26:38 +0530

    imagecurved

--- unity_world/lib/pages/Department/dns_dept_page.dart
+++ unity_world/lib/pages/Department/dns_dept_page.dart
@@ -123,11 +123,14 @@ class DnsDeptPage extends StatelessWidget {
                     children: [
                       Column(
                         children: [
-                          Image.asset(
+                           ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                          child: Image.asset(
                             'assets/images/chamara.webp',
                             width: 100,
                             height: 100,
                           ),
+                           ),
                           const Text(
                             'Mr. Chamara Disanayake\nSenior Lecturer',
                             textAlign: TextAlign.center,
