1f0dc70f09524f1daccec6b46c0ae2b0f1a079ca
Yasod Kavindu de Silva <97452320+Yasodkai11@users.noreply.github.com>
2023-09-18 11:07:23 +0530

    containercurved

--- unity_world/lib/pages/Department/dns_dept_page.dart
+++ unity_world/lib/pages/Department/dns_dept_page.dart
@@ -19,6 +19,8 @@ class DnsDeptPage extends StatelessWidget {
                     height: 200,
                     width: 1000,
                   ),
+                  _buildCurvedContainer(
+                    context,
                   Container(
                     width: 300,
                     padding: EdgeInsets.all(20),
@@ -28,6 +30,8 @@ class DnsDeptPage extends StatelessWidget {
                     ),
                     color: Color.fromARGB(255, 15, 26, 88),
                   ),
+                  ),
+
                   SizedBox(height: 10),
                   Container(
                     width: 300,
@@ -172,6 +176,18 @@ class DnsDeptPage extends StatelessWidget {
     );
     
     
+  }
+  Widget _buildCurvedContainer(BuildContext context, Widget child) {
+    return Container(
+      margin:
+       EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius:
+         BorderRadius.circular(20), // Adjust the radius as needed
+        color: Color.fromARGB(255, 15, 26, 88),
+      ),
+      child: child,
+    );
   }
   Widget _buildProgramLink(
       BuildContext context, String programName, String url) {
