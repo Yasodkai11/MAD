017d056f65276400b8848cfaed6f08559afd0bcd
Sanali Losathi <kklosathi19@gmail.com>
2023-09-18 09:12:08 +0530

    added drawer

--- unity_world/lib/components/drawer.dart
+++ unity_world/lib/components/drawer.dart
@@ -16,7 +16,47 @@ class MyDrawer extends StatelessWidget {
         bottomRight: Radius.circular(15),
       ),
       
-      child: Drawer()
+      child: Drawer(
+
+        child: ListView(
+
+          padding: EdgeInsets.zero,
+          children: [
+
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(15, 26, 88, 1.0),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                ),
+              ),
+
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/image/logo1.png',
+                    width: size.width * 0.5,
+                    height: size.height * 0.12,
+                  ),
+
+                  Text(
+                    "YASHOD SILVA",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              )
+            ),
+
+            
+          ],
+        ),
+      )
     );
   }
   
