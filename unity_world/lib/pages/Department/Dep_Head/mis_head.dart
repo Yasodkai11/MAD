18b1f611089961ac427706a4ef35b68a7070def2
Kavee2000 <99233218+Kavee2000@users.noreply.github.com>
2023-09-19 10:31:15 -0700

    mis7

--- unity_world/lib/pages/Department/Dep_Head/mis_head.dart
+++ unity_world/lib/pages/Department/Dep_Head/mis_head.dart
@@ -83,3 +83,15 @@ class MisheadPage extends StatelessWidget {
     );
   }
 }
+
+
+Widget _buildCurvedContainer(BuildContext context, Widget child) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 10),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(20), // Adjust the radius as needed
+      color: Color.fromARGB(255, 15, 26, 88),
+    ),
+    child: child,
+  );
+}
