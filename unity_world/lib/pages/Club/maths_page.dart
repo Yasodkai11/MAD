02df64393e1fdf15b92cb3b460e97d561d03027a
nedmi16 <nedmi.mymail@gmail.com>
2023-09-19 13:55:05 +0530

    modified math

--- unity_world/lib/pages/Club/maths_page.dart
+++ unity_world/lib/pages/Club/maths_page.dart
@@ -41,3 +41,15 @@ lass MathsPage extends StatelessWidget {
     );
   }
 }
+
+
+class DescriptionBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 15, 26, 88),
+        borderRadius: BorderRadius.circular(10),
+      ),
