091d51f7d96b445d66129cafc898bfa93342e10c
Kavee2000 <99233218+Kavee2000@users.noreply.github.com>
2023-09-19 10:09:53 -0700

    mis2

--- unity_world/lib/pages/Department/Dep_Head/mis_head.dart
+++ unity_world/lib/pages/Department/Dep_Head/mis_head.dart
@@ -7,3 +7,19 @@ class MisheadPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Dr. Mohamed Shafraz'),
       ),
+
+       body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Dr. Mohamed Shafraz',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+
+            
