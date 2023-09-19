eec8dfdb06c7ebe6c8a8068c1bdd48677688eab6
Kavee2000 <99233218+Kavee2000@users.noreply.github.com>
2023-09-19 10:05:37 -0700

    mis1

---
+++ unity_world/lib/pages/Department/Dep_Head/mis_head.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MisheadPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dr. Mohamed Shafraz'),
+      ),
