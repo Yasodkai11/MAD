de65e2df0f1ce0b14a7d76d10490b37bbd81b402
Kavee2000 <99233218+Kavee2000@users.noreply.github.com>
2023-09-22 10:53:07 -0700

    app theme

---
+++ unity_world/lib/pages/SettingPage/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  
+  static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
